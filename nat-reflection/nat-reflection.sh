bd29f26f73b19a066facb54121f405a519303fab
ctzoki <ctzoki@protonmail.com>
2023-06-21 20:59:52 +0400

    change the chain to output instead of prerouting

--- nat-reflection/nat-reflection.sh
+++ nat-reflection/nat-reflection.sh
@@ -15,7 +15,7 @@ mkdir -p $RULES_PATH
 function add_rule() {
   local ip=$1
   local dest=$2
-  iptables -t nat -A PREROUTING -d $ip -j DNAT --to-destination $dest
+  iptables -t nat -A OUTPUT -d $ip -j DNAT --to-destination $dest
   echo "$ip $dest" >> "$RULES_PATH/rules"
 }
 
@@ -26,8 +26,8 @@ function list_rules() {
 function delete_rule() {
   local ip=$1
   local dest=$2
-  if iptables -t nat -C PREROUTING -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1; then
-    iptables -t nat -D PREROUTING -d $ip -j DNAT --to-destination $dest
+  if iptables -t nat -C OUTPUT -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1; then
+    iptables -t nat -D OUTPUT -d $ip -j DNAT --to-destination $dest
   fi
   sed -i "/$ip $dest/d" "$RULES_PATH/rules"
 }
@@ -35,10 +35,10 @@ function delete_rule() {
 function reload_rules() {
   while IFS=' ' read -r line; do
     IFS=' ' read -r ip dest <<<"$line"
-    if iptables -t nat -C PREROUTING -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1; then
-      iptables -t nat -D PREROUTING -d $ip -j DNAT --to-destination $dest
+    if iptables -t nat -C OUTPUT -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1; then
+      iptables -t nat -D OUTPUT -d $ip -j DNAT --to-destination $dest
     fi
-    iptables -t nat -A PREROUTING -d $ip -j DNAT --to-destination $dest
+    iptables -t nat -A OUTPUT -d $ip -j DNAT --to-destination $dest
   done < "$RULES_PATH/rules"
 }
 
